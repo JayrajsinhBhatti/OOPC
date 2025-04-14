e5ea7b634871ef4d3f6271ce7dc422a32e71eff9
Jayrajsinh Bhatti <jayrajsinhbhatti9687@gmail.com>
2025-04-14 15:00:56 +0530

    QUESTION 5.1

--- 24CE014_Q.5.1.cpp
+++ 24CE014_Q.5.1.cpp
@@ -1,22 +1,20 @@
 #include<iostream>
 #include<string>
 #include<vector>
-#include<map>
 using namespace std;
 
 class Calculator {
+    int fint, sint;
+    float ffloat, sfloat;
+
     public:
     void GetNumber(int ia, int ib) {
-        cout<<"Enter first number: ";
-        cin>>ia;
-        cout<<"Enter second number: ";
-        cin>>ib;
+        ia = fint;
+        ib = sint;
     }
     void GetNumber(float fa, float fb) {
-        cout<<"Enter first number: ";
-        cin>>fa;
-        cout<<"Enter second number: ";
-        cin>>fb;
+        fa = ffloat;
+        fb = sfloat;
     }
     void Sum(int ia, int ib) {
         cout<<ia+ib;
@@ -33,13 +31,23 @@ int main()
 
     Calculator c;
 
-    cout<<"Eneter 1-int, 2-float";
+    cout<<"Enter 1-int, 2-float";
     cin>>choice;
     if (choice==1)
     {
+        cout<<"Enter first int number: ";
+        cin>>ia;
+        cout<<"Enter second int number: ";
+        cin>>ib;
+
         c.GetNumber(ia, ib);
         c.Sum(ia, ib);
     } else if(choice==2){
+        cout<<"Enter first float number: ";
+        cin>>fa;
+        cout<<"Enter second float number: ";
+        cin>>fb;
+
         c.GetNumber(fa, fb);
         c.Sum(fa, fb);
     } else {
