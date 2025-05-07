b66a8954a9038b98a33c74d747c7130a03528fee
Jayrajsinh Bhatti <jayrajsinhbhatti9687@gmail.com>
2025-05-07 09:45:48 +0530

    Add files via upload

--- 24CE014_Q.5.2.cpp
+++ 24CE014_Q.5.2.cpp
@@ -2,29 +2,79 @@
 using namespace std;
 
 class Complex {
-    int RealPart, ImaginaryPart;
+private:
+    float Real, Imag;
 
-    public:
-    void InputNums() {
-        cout<<"Enter real part: ";
-        cin>>RealPart;
-        cout<<"Enter Imaginary part: ";
-        cin>>ImaginaryPart;
+public:
+    void setData(float r, float i) {
+        this->Real = r;
+        this->Imag = i;
     }
-    void operator+(Complex &b) {
-        Complex ans;
-        ans.RealPart = RealPart+b.RealPart;
-        ans.ImaginaryPart = ImaginaryPart+b.ImaginaryPart;
-        cout<<RealPart << "+" <<ImaginaryPart<<"i + "<<b.RealPart<<"+ "<<b.ImaginaryPart<<"i = " <<ans.RealPart<<"+"<<ans.ImaginaryPart<<"i"; 
+
+    Complex operator+(Complex &other) {
+        Complex result;
+        result.Real = this->Real + other.Real;
+        result.Imag = this->Imag + other.Imag;
+        return result;
+    }
+
+    Complex operator-(Complex &other) {
+        Complex result;
+        result.Real = this->Real - other.Real;
+        result.Imag = this->Imag - other.Imag;
+        return result;
+    }
+
+    Complex operator*(Complex &other) {
+        Complex result;
+        result.Real = (this->Real * other.Real) - (this->Imag * other.Imag);
+        result.Imag = (this->Real * other.Imag) + (this->Imag * other.Real);
+        return result;
+    }
+
+    void display() {
+        cout << Real << " + i(" << Imag << ")";
     }
 };
 
-int main()
-{
-    Complex a,b;
-    a.InputNums();
-    b.InputNums();
-    a+b;
+int main() {
+    int choice = 1;
+    float r, i;
+    Complex a, b, c;
 
+    do {
+        cout << "\nEnter Complex Number A:" << endl;
+        cout << "Real part: ";
+        cin >> r;
+        cout << "Imaginary part: ";
+        cin >> i;
+        a.setData(r, i);
+
+        cout << "\nEnter Complex Number B:" << endl;
+        cout << "Real part: ";
+        cin >> r;
+        cout << "Imaginary part: ";
+        cin >> i;
+        b.setData(r, i);
+
+        cout << "\nResults:";
+
+        c = a + b;
+        cout << "\nA + B = ";
+        c.display();
+
+        c = a - b;
+        cout << "\nA - B = ";
+        c.display();
+
+        c = a * b;
+        cout << "\nA * B = ";
+        c.display();
+
+        cout << "\n\nDo you want to perform operations again? (1 = Yes, 0 = No): ";
+        cin >> choice;
+    } while (choice == 1);
+
+    cout << "\n24CE014 JAYRAJSINH BHATTI" << endl;
     return 0;
 }
