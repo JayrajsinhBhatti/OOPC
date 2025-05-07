00c94780d973f40d7c55ed949eea138f33fdc393
Jayrajsinh Bhatti <jayrajsinhbhatti9687@gmail.com>
2025-05-07 22:22:43 +0530

    Add files via upload

---
+++ 24CE014_Q.8.4.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int score;
+
+public:
+    Student(const string& n, int s) {
+        name = n;
+        score = s;
+    }
+
+    string getName() const {
+        return name;
+    }
+
+    int getScore() const {
+        return score;
+    }
+
+    void setName(const string& n) {
+        name = n;
+    }
+
+    void setScore(int s) {
+        score = s;
+    }
+};
+
+void inputStudents(int n, vector<Student>& students) {
+    cout << "Enter student name and score:" << endl;
+    for (int i = 0; i < n; ++i) {
+        string name;
+        int score;
+        cin >> name >> score;
+        students.push_back(Student(name, score));
+    }
+}
+
+void displayRankedStudents(const vector<Student>& students) {
+    cout << "Ranked list of students:" << endl;
+    int rank = 1;
+    for (const auto& student : students) {
+        cout << rank << ". " << student.getName() << " - " << student.getScore() << endl;
+        ++rank;
+    }
+}
+
+void sortStudentsByScore(vector<Student>& students) {
+    sort(students.begin(), students.end(), [](const Student& a, const Student& b) {
+        return a.getScore() > b.getScore();
+    });
+}
+
+int main() {
+    char choice;
+
+    do {
+        int n;
+        cout << "\nEnter number of students: ";
+        cin >> n;
+
+        vector<Student> students;
+        inputStudents(n, students);
+        sortStudentsByScore(students);
+        displayRankedStudents(students);
+
+        cout << "\nDo you want to enter another list? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    cout << "Thank you!" << endl;
+    cout<<"\n24CE014 JAYRAJSINH BHATTI";
+    return 0;
+}
