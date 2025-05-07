768cd848ccfeb81635cfba0e77a706e00923533e
Jayrajsinh Bhatti <jayrajsinhbhatti9687@gmail.com>
2025-05-07 10:01:27 +0530

    QUESTION 5.4

--- 24CE014_Q.5.4.cpp
+++ 24CE014_Q.5.4.cpp
@@ -4,33 +4,38 @@ using namespace std;
 class Celsius {
     float CelsiusVar;
 
-    public :
-
+public:
     Celsius(float CelVal) {
         CelsiusVar = CelVal;
     }
 
     float ConvertCelsiusToFahr() {
-        return CelsiusVar*33.8;
+        return (CelsiusVar * 9/5) + 32;
     }
 };
 
 class Fahrenheit {
     float FahrenheitVar;
 
-    public :
-
+public:
     Fahrenheit(float FahVar) {
         FahrenheitVar = FahVar;
     }
 
-    float FahrToCelsius(float Val) {
-        return Val*(9/5)+32;
+    float FahrToCelsius() {
+        return (FahrenheitVar - 32) * 5/9;
     }
 };
 
-int main()
-{
-    
+int main() {
+    float celsiusValue = 25;
+    float fahrenheitValue = 77;
+
+    Celsius c(celsiusValue);
+    Fahrenheit f(fahrenheitValue);
+
+    cout << celsiusValue << " Celsius is equal to " << c.ConvertCelsiusToFahr() << " Fahrenheit." << endl;
+    cout << fahrenheitValue << " Fahrenheit is equal to " << f.FahrToCelsius() << " Celsius." << endl;
+
     return 0;
 }
